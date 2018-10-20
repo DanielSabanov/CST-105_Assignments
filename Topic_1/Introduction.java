a719a79806c1133bb9df765785a9c037e42a11e8
DESKTOP-2B6P06E\danie <daniel@sadogursky.com>
2018-10-19 22:11:29 -0700

    Replaced a jar. file with the JAVA file.

---
+++ Topic_1/Introduction.java
@@ -0,0 +1,10 @@
+//By Daniel Sabanov
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Daniel Sabanov.");
+        System.out.println("My favorite color is maroon.");
+        System.out.println("If I won the lottery, I would invest the money.");
+    }
+}
